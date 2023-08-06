4b5ba9d1da2500908153cd8874dfb28379a5c733
tobi-rojas <tobirojas26@gmail.com>
2023-08-05 22:21:30 -0300

    nuevo

---
+++ javascript/main.js
@@ -0,0 +1,14 @@
+/*Alt + Shift copio en linea bajo con flecha 
+Alt mantener para escribir lo mismo en distintos lados
+Cntrl Shift Z */
+
+let nombre = prompt("Ingresa tu nombre")
+let saludo = "Hola"
+alert(saludo + " " + nombre)
+
+let numero = Number(prompt("Ingresa un numero que se sumara al 10"))
+alert(numero + 10)
+
+let nombre1 = prompt("Ingrese su primer nombre")
+let nombre2 = prompt("Ingrese su segundo nombre")
+alert(nombre1 + " " + nombre2)
